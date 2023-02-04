d3c9288c94b390aa9e6683f3dbb73a00fc054fe7
Yous113 <44925411+Yous113@users.noreply.github.com>
2023-02-04 19:44:11 +0100

    finish finish

--- Bot/eventbot.py
+++ Bot/eventbot.py
@@ -38,7 +38,7 @@ async def on_message(message):
 
     if contents.startswith("!SetEvent"):
       lister.title = contents[10:]
-      firstreply = "Write some date you would like to hold the event: "+ lister.title +", start with '!Date' then write your dates and end with '!Done'"
+      firstreply = "Write some date you would like to hold the event: "+ lister.title +", use command '!Date' then write your dates and end with '!Done' (Remember ',' between every date)"
       # Create an object of the class event
       lister.title = lister.Event(lister.title, [])
       lister.setEvent = True
@@ -54,18 +54,20 @@ async def on_message(message):
           dates = con.split(",")
           # Sets the eventDates(list) value of the class to the given dates
           lister.title.eventDates = dates
+          await message.channel.send("React on the date that fit you best!")
           #for loop looks in the list of dates
           for date in dates:
             # sends a message of each date
             m = await message.channel.send(date)
             # Puts all of the message ids in a dictionary
             lister.ReactDict[m.id] = 0
+          await message.channel.send("When the voting is finished use command '!SetDate' to determine the best date")
           lister.setEvent = False
 
     if contents.startswith("!SetDate"):
       # Checks if the dictionary is empty
       if len(lister.ReactDict) == 0:
-        firstreply = "Giv nogen datoer for eventet:" + lister.title +",ved brug af kommando '!date' og afslut med '!done'"
+        firstreply = "Write some date you would like to hold the event: "+ lister.title +", use command '!Date' then write your dates and end with '!Done' (Remember ',' between every date)"
         await message.channel.send(firstreply)
       else:
         # Looks for the biggest value in the dictionary
